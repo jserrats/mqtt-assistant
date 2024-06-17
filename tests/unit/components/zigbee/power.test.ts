6b0bb1f9a887a52b9571dc94c3fd18a09b008efb
jserrats <jaume.serrats9@gmail.com>
2024-06-17 21:54:16 +0200

    added tests

---
+++ tests/unit/components/zigbee/power.test.ts
@@ -0,0 +1,40 @@
+import { router } from "../../../../src/router"
+import { client } from "../../../../src/mqtt"
+import { WattPowerZigbee } from "../../../../src/components/zigbee/power"
+
+jest.mock('../../../../src/mqtt', () => ({
+    client: {
+        publish: jest.fn((newTopic: string, newPayload: string) => { router.route(newTopic, newPayload) })
+    }
+}));
+
+describe('Power', () => {
+    var wattPower: WattPowerZigbee
+
+
+    beforeAll(async () => {
+        wattPower = new WattPowerZigbee("test1")
+    })
+
+    afterEach(async () => {
+        (client.publish as jest.Mock).mockClear()
+
+    })
+
+    it('should update status', async () => {
+        client.publish(wattPower.topic, JSON.stringify({ power: 43.57, state: "ON", }))
+        expect(wattPower.state).toBe(true)
+        client.publish(wattPower.topic, JSON.stringify({ power: 43.57, state: "OFF", }))
+        expect(wattPower.state).toBe(false)
+    })
+
+    it('should update power', async () => {
+        client.publish(wattPower.topic, JSON.stringify({ power: 43.57, state: "OFF", }))
+        expect(wattPower.power).toBe(43.57)
+    })
+
+    it('should turn on/off', async () => {
+        wattPower.on()
+        expect((client.publish as jest.Mock).mock.calls[0]).toStrictEqual([wattPower.setTopic, 'ON'])
+    })
+})
