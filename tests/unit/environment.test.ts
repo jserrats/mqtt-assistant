dc7f3a481afa64ed422d9957abf76a2f77a96a16
jserrats <jaume.serrats9@gmail.com>
2024-07-13 14:27:21 +0200

    env test

--- tests/unit/environment.test.ts
+++ tests/unit/environment.test.ts
@@ -1,14 +1,19 @@
 import { getEnvVariable } from "../../src/environment";
 
-
 describe("index", () => {
 	it("should throw an error when the env does not exist", async () => {
-		const t = () => { getEnvVariable("681e567f-8032-45b4-acea-618a6c28bd72") }
-		expect(t).toThrow("Missing environment variable 681e567f-8032-45b4-acea-618a6c28bd72")
+		const t = () => {
+			getEnvVariable("681e567f-8032-45b4-acea-618a6c28bd72");
+		};
+		expect(t).toThrow(
+			"Missing environment variable 681e567f-8032-45b4-acea-618a6c28bd72",
+		);
 	});
 
 	it("should retrieve env variables", async () => {
-		process.env["681e567f-8032-45b4-acea-618a6c28bd72"] = "TEST_VALUE"
-		expect(getEnvVariable("681e567f-8032-45b4-acea-618a6c28bd72")).toStrictEqual("TEST_VALUE")
+		process.env["681e567f-8032-45b4-acea-618a6c28bd72"] = "TEST_VALUE";
+		expect(
+			getEnvVariable("681e567f-8032-45b4-acea-618a6c28bd72"),
+		).toStrictEqual("TEST_VALUE");
 	});
 });
