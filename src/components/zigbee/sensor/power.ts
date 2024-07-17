f7e7c84d3a56252daff3dec04abe30aad74e248b
jserrats <jaume.serrats9@gmail.com>
2024-07-17 18:40:47 +0200

    added PowerSensorZigbee

---
+++ src/components/zigbee/sensor/power.ts
@@ -0,0 +1,27 @@
+import { router } from "../../../router";
+import type { InboundZigbeeInfo } from "../zigbee";
+import { GenericZigbeeSensor } from "./sensor";
+
+export class PowerSensorZigbee extends GenericZigbeeSensor {
+	power?: number;
+	voltage?: number;
+	current?: number;
+
+	trigger = {
+		all: { topic: this.triggerTopic, payload: "*" },
+	};
+
+	updateComponent(message: PowerSensorZigbeeComponentInfo): void {
+		this.power = message.power;
+		this.voltage = message.voltage;
+		this.current = message.current;
+
+		super.updateComponent(message);
+	}
+}
+
+export type PowerSensorZigbeeComponentInfo = {
+	power: number;
+	voltage: number;
+	current: number;
+} & InboundZigbeeInfo;
