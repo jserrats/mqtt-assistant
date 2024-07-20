201f2e16d5ecabe123ca180caacb645eb98d7244
jserrats <jaume.serrats9@gmail.com>
2024-07-20 12:23:55 +0200

    improved telegram logging

--- src/components/telegram.ts
+++ src/components/telegram.ts
@@ -1,13 +1,54 @@
 import { Component } from "./component";
+const { dirname } = require("node:path");
 
 class Telegram extends Component {
+	static base_topic = "notify/telegram";
+
+	/**
+	 * Basic method for sending error messages without formatting
+	 * @param string to be logged on telegram
+	 */
 	send(message: string) {
-		this.client.publish("notify/telegram", message);
+		this.client.publish(Telegram.base_topic, message);
 	}
 
-	sendError(error: Error) {
-		this.send(error.message);
+	/**
+	 * 
+	 * @param error Error object that needs to be logged
+	 */
+	logError(error: Error) {
+		this.log(
+			{
+				message: error.message,
+				name: error.name,
+				// TODO: send the root project folder name
+				service: dirname(require.main.filename),
+			} as TelegramErrorMessage,
+			"error",
+		);
+	}
+
+	log(message: TelegramMessage | TelegramErrorMessage, logLevel?: LogLevel) {
+		let topic = Telegram.base_topic;
+		if (logLevel !== undefined) {
+			topic = `${topic}/${logLevel}`;
+		}
+		this.client.publish(topic, JSON.stringify(message));
 	}
 }
 
+export type LogLevel = "debug" | "info" | "warning" | "error";
+
 export const telegram = new Telegram();
+
+export type TelegramMessage = {
+	title?: string;
+	message: string;
+	recipient?: "admin" | "home";
+};
+
+export type TelegramErrorMessage = {
+	name: string;
+	message: string;
+	service: string;
+};
---
+++ src/components/tests/telegram.test.ts
@@ -0,0 +1,74 @@
+import { client } from "../../mqtt";
+import { router } from "../../router";
+import {
+	type TelegramErrorMessage,
+	type TelegramMessage,
+	telegram,
+} from "../telegram";
+
+jest.mock("../../../src/mqtt", () => ({
+	client: {
+		publish: jest.fn((newTopic: string, newPayload: string) => {
+			router.route(newTopic, newPayload);
+		}),
+	},
+}));
+
+describe("Telegram", () => {
+	afterEach(async () => {
+		(client.publish as jest.Mock).mockClear();
+	});
+
+	it("should send a message", async () => {
+		telegram.send("test");
+		expect((client.publish as jest.Mock).mock.calls[0][0]).toStrictEqual(
+			"notify/telegram",
+		);
+		expect((client.publish as jest.Mock).mock.calls[0][1]).toStrictEqual(
+			"test",
+		);
+	});
+
+	it("should send an error message", async () => {
+		telegram.logError(new EvalError("test error"));
+		expect((client.publish as jest.Mock).mock.calls[0][0]).toStrictEqual(
+			"notify/telegram/error",
+		);
+		console.log((client.publish as jest.Mock).mock.calls[0][1]);
+		expect(
+			JSON.parse(
+				(client.publish as jest.Mock).mock.calls[0][1],
+			) as TelegramErrorMessage,
+		).toMatchObject({
+			message: "test error",
+			name: "EvalError",
+		} as TelegramErrorMessage);
+	});
+
+	it("should send a message with log level", async () => {
+		telegram.log({ message: "test" }, "debug");
+		expect((client.publish as jest.Mock).mock.calls[0][0]).toStrictEqual(
+			"notify/telegram/debug",
+		);
+
+		const inbound = JSON.parse(
+			(client.publish as jest.Mock).mock.calls[0][1],
+		) as TelegramMessage;
+
+		expect(inbound.message === "test").toBeTruthy();
+	});
+
+	it("should send a message with recipient option", async () => {
+		telegram.log({ message: "test1", recipient: "home" });
+		expect((client.publish as jest.Mock).mock.calls[0][0]).toStrictEqual(
+			"notify/telegram",
+		);
+
+		const inbound = JSON.parse(
+			(client.publish as jest.Mock).mock.calls[0][1],
+		) as TelegramMessage;
+
+		expect(inbound.message === "test1").toBeTruthy();
+		expect(inbound.recipient === "home").toBeTruthy();
+	});
+});
