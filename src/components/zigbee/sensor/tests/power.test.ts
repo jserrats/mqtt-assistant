3ae13225ccf0c59bab2eddc5fd86f774848d7467
jserrats <jaume.serrats9@gmail.com>
2024-07-22 18:13:52 +0200

    power test

---
+++ src/components/zigbee/sensor/tests/power.test.ts
@@ -0,0 +1,39 @@
+import { router } from "../../../../router";
+import {
+	 PowerSensorZigbee,
+	type PowerSensorZigbeeComponentInfo,
+} from "../power";
+
+jest.mock("../../../../mqtt", () => ({
+	client: {
+		publish: jest.fn((newTopic: string, newPayload: string) => {}),
+	},
+}));
+
+describe("PowerSensorZigbee", () => {
+	it("should trigger the callback", async () => {
+		const mockCallback = jest.fn();
+
+		const sensor = new PowerSensorZigbee("test", {
+			updateCallback: mockCallback,
+		});
+		router.route(
+			sensor.topic,
+			JSON.stringify({ power: 10 } as PowerSensorZigbeeComponentInfo),
+		);
+		expect(mockCallback).toHaveBeenCalled();
+	});
+
+	it("should update the power correctly", async () => {
+		const sensor = new PowerSensorZigbee("test2");
+		expect(sensor.power).toBeUndefined();
+
+		router.route(
+			sensor.topic,
+			JSON.stringify({
+				power: 10
+			} as PowerSensorZigbeeComponentInfo),
+		);
+		expect(sensor.power).toStrictEqual(10);
+	});
+});
