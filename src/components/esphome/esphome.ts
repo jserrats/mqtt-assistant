20d95d2ff188e131128c841478bdeabaf19fc754
jserrats <jaume.serrats9@gmail.com>
2024-07-25 22:55:35 +0200

    better naming

--- src/components/esphome/esphome.ts
+++ src/components/esphome/esphome.ts
@@ -4,13 +4,17 @@ import { Component } from "../component";
 export class ESPHomeComponent extends Component {
 	protected baseTopic: string;
 	protected stateTopic: string;
+
+	/**
+	 * Human readable name
+	 */
 	name: string;
 
 	constructor(name: string, component: string, type: ESPHomeComponentTypes) {
 		super();
 		this.baseTopic = ESPHOME_TOPIC + name;
 		this.stateTopic = `${this.baseTopic}/${type}/${component}/state`;
-		this.name = name;
+		this.name = `${name}:${component}`;
 	}
 }
 
