5e56c8effa8f6bf6abee6fdc39c16dd70c167da3
jserrats <jaume.serrats9@gmail.com>
2024-08-01 20:36:54 +0200

    added switch test

--- src/components/esphome/switch.ts
+++ src/components/esphome/switch.ts
@@ -1,26 +1,25 @@
 import { router } from "../../router";
-import { type Automation, Trigger } from "../../types";
+import type { Automation, Trigger } from "../../types";
 import { ESPHomeComponent } from "./esphome";
 
 export class SwitchESPHome extends ESPHomeComponent {
 	commandTopic: string;
-	state = false;
+	state: boolean;
 	updater: Automation;
 
 	trigger = {
-		on: { topic: "", payload: "ON" },
-		off: { topic: "", payload: "OFF" },
+		on: { topic: this.stateTopic, payload: "ON" },
+		off: { topic: this.stateTopic, payload: "OFF" },
+		all: { topic: this.stateTopic, payload: "*" },
 	};
 
 	constructor(name: string, component: string) {
-		super(name, component, "light");
+		super(name, component, "switch");
 		this.commandTopic = `${this.baseTopic}/switch/${component}/command`;
-		this.trigger.off.topic = this.stateTopic;
-		this.trigger.on.topic = this.stateTopic;
 		this.updater = {
-			trigger: { topic: this.baseTopic, payload: "*" },
-			callback: (message: string) => {
-				this.updateComponent(message);
+			trigger: { topic: this.stateTopic, payload: "*" },
+			callback: (message: Trigger) => {
+				this.updateComponent(message.payload);
 			},
 		};
 		router.addAutomation(this.updater);
@@ -34,8 +33,18 @@ export class SwitchESPHome extends ESPHomeComponent {
 		this.set(false);
 	}
 
-	private set(status: boolean) {
-		this.client.publish(this.commandTopic, status ? "ON" : "OFF");
+	toggle() {
+		this.set("toggle");
+	}
+
+	private set(order: boolean | "toggle") {
+		let text_order: string;
+		if (typeof order === "boolean") {
+			text_order = order ? "ON" : "OFF";
+		} else {
+			text_order = "TOGGLE";
+		}
+		this.client.publish(this.commandTopic, text_order);
 	}
 
 	private updateComponent(message: string) {
---
+++ src/components/esphome/tests/switch.test.ts
@@ -0,0 +1,47 @@
+import { client } from "../../../mqtt";
+import { router } from "../../../router";
+import { SwitchESPHome } from "../switch";
+
+jest.mock("../../../mqtt", () => ({
+	client: {
+		publish: jest.fn((newTopic: string, newPayload: string) => {}),
+	},
+}));
+
+describe("SwitchESPHome", () => {
+	let switchEsphome: SwitchESPHome;
+
+	beforeAll(async () => {
+		switchEsphome = new SwitchESPHome("test1", "test1");
+	});
+
+	afterEach(async () => {
+		(client.publish as jest.Mock).mockClear();
+	});
+
+	it("should update status", async () => {
+		expect(switchEsphome.state).toBe(undefined);
+
+		router.route(switchEsphome.trigger.on.topic, "ON");
+		expect(switchEsphome.state).toBeTruthy();
+
+		router.route(switchEsphome.trigger.off.topic, "OFF");
+		expect(switchEsphome.state).toBe(false);
+	});
+
+	it("should turn on/off", async () => {
+		switchEsphome.on();
+		expect((client.publish as jest.Mock).mock.calls[0]).toStrictEqual([
+			switchEsphome.commandTopic,
+			"ON",
+		]);
+	});
+
+	it("should toggle", async () => {
+		switchEsphome.toggle();
+		expect((client.publish as jest.Mock).mock.calls[0]).toStrictEqual([
+			switchEsphome.commandTopic,
+			"TOGGLE",
+		]);
+	});
+});
