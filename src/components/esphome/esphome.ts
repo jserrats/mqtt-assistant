8845635380e0390cfc7552943a1dbea93dc9bb2e
jserrats <jaume.serrats9@gmail.com>
2024-09-07 19:17:25 +0200

    esphome monitoring

--- src/components/esphome/esphome.ts
+++ src/components/esphome/esphome.ts
@@ -1,5 +1,8 @@
 import { ESPHOME_TOPIC } from "../../topics";
 import { Component } from "../component";
+import { router } from "../../router";
+import { Trigger } from "../../types";
+import { telegram } from "../telegram";
 
 export class ESPHomeComponent extends Component {
 	protected baseTopic: string;
@@ -23,3 +26,51 @@ export type ESPHomeComponentTypes =
 	| "light"
 	| "sensor"
 	| "binary_sensor";
+
+export class EsphomeMonitor extends Component {
+	offlineDevices: string[] = [];
+	ignoredDevices: string[] = [];
+
+	constructor(ignoredDevices?: string[]) {
+		super();
+
+		ignoredDevices.forEach((device: string) => {
+			this.ignoredDevices.push(`${ESPHOME_TOPIC}${device}/status`);
+		});
+
+		router.addAutomation({
+			trigger: { topic: `${ESPHOME_TOPIC}*/status`, payload: "*" },
+			callback: (message: Trigger) => {
+				if (this.ignoredDevices.includes(message.topic)) {
+					return;
+				}
+				const payload = message.payload as InboundAvailability;
+				if (payload === "offline") {
+					this.sendNotification(message);
+					this.offlineDevices.push(message.topic);
+				} else if (
+					payload === "online" &&
+					this.offlineDevices.includes(message.topic)
+				) {
+					this.sendNotification(message);
+					this.offlineDevices.splice(
+						this.offlineDevices.indexOf(message.topic),
+						1,
+					);
+				}
+			},
+		});
+	}
+
+	sendNotification(message: Trigger) {
+		telegram.log(
+			{
+				title: `ESPHome component \`${message.topic.split("/")[1]}\``,
+				message: `status: \`${message.payload as InboundAvailability}\``,
+			},
+			"warning",
+		);
+	}
+}
+
+export type InboundAvailability = "online" | "offline";
---
+++ src/components/esphome/tests/esphome.test.ts
@@ -0,0 +1,52 @@
+import { client } from "../../../mqtt";
+import { router } from "../../../router";
+import { ESPHOME_TOPIC } from "../../../topics";
+import { EsphomeMonitor, InboundAvailability } from "../esphome";
+
+jest.mock("../../../../src/mqtt", () => ({
+	client: {
+		publish: jest.fn((newTopic: string, newPayload: string) => {
+			router.route(newTopic, newPayload);
+		}),
+	},
+}));
+
+
+describe("ESPHomeMonitor", () => {
+	let esphomeMonitor: EsphomeMonitor;
+
+	beforeAll(async () => {
+		esphomeMonitor = new EsphomeMonitor(["ignored"]);
+	});
+
+	afterEach(async () => {
+		(client.publish as jest.Mock).mockClear();
+	});
+
+	it("should notify when a ESPHome component disconnects", async () => {
+		client.publish(`${ESPHOME_TOPIC}test1/status`, "offline" as InboundAvailability);
+		expect((client.publish as jest.Mock).mock.calls[1][0]).toStrictEqual(
+			"notify/telegram/warning",
+		);
+	});
+
+	it("should not notify when a ESPHome component is online without being offline before", async () => {
+		client.publish(`${ESPHOME_TOPIC}test2/status`, "online" as InboundAvailability);
+		expect((client.publish as jest.Mock).mock.calls).toHaveLength(1);
+	});
+
+	it("should notify when a ESPHome component connects after being disconnected", async () => {
+		client.publish(`${ESPHOME_TOPIC}test1/status`, "online" as InboundAvailability);
+		expect((client.publish as jest.Mock).mock.calls).toHaveLength(2);
+	});
+
+	it("should not notify again", async () => {
+		client.publish(`${ESPHOME_TOPIC}test1/status`, "online" as InboundAvailability);
+		expect((client.publish as jest.Mock).mock.calls).toHaveLength(1);
+	});
+
+	it("should ignore blacklisted devices", async () => {
+		client.publish(`${ESPHOME_TOPIC}ignored/status`, "offline" as InboundAvailability);
+		expect((client.publish as jest.Mock).mock.calls).toHaveLength(1);
+	});
+});
